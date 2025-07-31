a311b1b2a88667d2699b4de05d43d81081baf58a
DevMiguelAcevedo <acebedo2524@gmail.com>
2025-07-31 03:35:29 -0500

    Modificacion para deploy

--- config/cors.php
+++ config/cors.php
@@ -6,7 +6,7 @@ return [
     'allowed_origins' => [
         'http://localhost:5173',
         'https://actaexpress-client.vercel.app',
-        'https://actaexpress-client-git-master-miguel-acevedos-projects-ec296349.vercel.app',
+        'https://actaexpress-client-git-master-miguel-acevedos-projects-ec296349.vercel.app'
     ],
     'allowed_origins_patterns' => [],
     'allowed_headers' => ['*'],
