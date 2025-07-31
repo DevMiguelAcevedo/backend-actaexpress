1767af319d73fb9e17ef29bd4c9519427b1ec197
DevMiguelAcevedo <acebedo2524@gmail.com>
2025-07-31 02:13:36 -0500

    Modificacion de vite.config y vercel.json

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,32 @@
-import { defineConfig } from 'vite';
-import laravel from 'laravel-vite-plugin';
-import tailwindcss from '@tailwindcss/vite';
+import { defineConfig } from "vite";
+import laravel from "laravel-vite-plugin";
+import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
+    base: "/build/",
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
+            input: ["resources/css/app.css", "resources/js/app.js"],
             refresh: true,
         }),
         tailwindcss(),
     ],
+    build: {
+        manifest: true,
+        outDir: "public/build",
+        emptyOutDir: true,
+        rollupOptions: {
+            input: ["resources/css/app.css", "resources/js/app.js"],
+            output: {
+                entryFileNames: "[name].js",
+                chunkFileNames: "[name].js",
+                assetFileNames: "[name].[ext]",
+            },
+        },
+    },
+    server: {
+        hmr: {
+            host: "localhost",
+        },
+    },
 });
